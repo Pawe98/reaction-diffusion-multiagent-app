39dbe6491d3e14b94717cc2f6599d3074ee795c5
f9rken <furkan.boehringer@gmail.com>
2023-07-06 22:40:35 +0200

    add: gslLint script, default shader

--- src/shader/VertexShaderSource.js
+++ src/shader/VertexShaderSource.js
@@ -1,9 +1,9 @@
 import React from 'react'
 const VertexShaderSource = `
     attribute vec2 a_position;
-    varying vec2 vUv;
+    
     void main() {
-      vUv = uv;
+
       gl_Position = vec4(a_position, 0, 1);
     }
     `
